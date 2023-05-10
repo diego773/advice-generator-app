c5dd160a8107acbfd5d4fd98fe948da34b345ded
Diego <diegovillarreal123@gmail.com>
2023-05-10 14:16:01 -0500

    added desktop style and fixed pattern divider and also added glow

--- assets/script.js
+++ assets/script.js
@@ -1,39 +1,54 @@
-const button = document.querySelector(".button-circle");
+const button = document.getElementById("button-circle");
 
 const handleClick = () => {
-  getAdviceApi();
-  console.log("click");
+  if (!getAdviceApi) {
+    alert("sorry, no advice available");
+  }
+  return getAdviceApi();
 };
 
-const getAdviceApi = async (advice) => {
-  const apiUrl = "https://api.adviceslip.com/advice";
+const getAdviceApi = async () => {
+  const url = "https://api.adviceslip.com/advice";
   try {
-    const response = await fetch(apiUrl);
+    const response = await fetch(url);
     if (response.status !== 200) {
       console.log("Server error: ", response);
     }
     const data = await response.json();
     mainContainer(data);
-    console.log("data", data);
   } catch (error) {
     console.log("error: ", error);
   }
 };
 
-const mainContainer = (advice) => {
-  adviceIdSection(advice);
-  adviceSectionText(advice);
+const mainContainer = (data) => {
+  const message = `Sorry no ${adviceIdSection} and no ${adviceSectionText} found.`;
+
+  if (!adviceIdSection || !adviceSectionText) {
+    return message;
+  } else {
+    adviceIdSection(data);
+    adviceSectionText(data);
+    return;
+  }
 };
 
 const adviceIdSection = (data) => {
-  const adviceIdNumber = document.querySelector(".advice-id-number");
+  const adviceIdNumber = document.getElementById("advice-id-number");
+  if (!adviceIdNumber) {
+    adviceIdNumber.textContent = "";
+  }
   adviceIdNumber.textContent = `Advice # ${data.slip.id}`;
 };
 
-const adviceSectionText = (text) => {
-  const adviceText = document.querySelector(".advice-text");
-  adviceText.textContent = text.slip.advice;
+const adviceSectionText = (data) => {
+  const adviceText = document.getElementById("advice-text");
+
+  if (!adviceText) {
+    adviceText.textContent = "";
+  }
+  adviceText.textContent = `"${data.slip.advice}"`;
 };
 
-getAdviceApi("advice");
+getAdviceApi();
 button.addEventListener("click", handleClick);
